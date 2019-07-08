35eb745fef6780ae81a0db97c323483ae4525b74
Santiago Montoya Angarita <sntiagoma@gmail.com>
2019-07-08 09:16:11 -0500

    Cloud Code to prepopulate from WAZE API Venue

--- cloud/main.js
+++ cloud/main.js
@@ -22,3 +22,18 @@ Parse.Cloud.job('updateWazeKey', async request => {
   log(`Update WAZE_KEY to ${resp.data}`);
   return resp.data;
 });
+
+Parse.Cloud.beforeSave('Venue', async request => {
+  const wazePlaceId = request.object.get('wazePlaceId');
+  const config = await Parse.Config.get();
+  const wazeApiKey = config.get('WAZE_KEY');
+  const resp = await Parse.Cloud.httpRequest({
+    url: `https://www.waze.com/maps/api/place/details/json?key=${wazeApiKey}&placeid=${wazePlaceId}`
+  });
+  const { formatted_address: address, geometry, url } = resp.data.result;
+  const { location } = geometry;
+  const { lat, lng } = location;
+  request.object.set('address', address);
+  request.object.set('url', url);
+  request.object.set('location', new Parse.GeoPoint(+lat, +lng));
+});
